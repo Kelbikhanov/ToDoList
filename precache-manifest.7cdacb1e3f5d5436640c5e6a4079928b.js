9027fcccaa1a28b1a8affbddcc41387e771c957f
kelbikhanov <rusik9091@gmail.com>
2020-02-26 06:12:52 +0300

    Updates

---
+++ precache-manifest.7cdacb1e3f5d5436640c5e6a4079928b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64b592b6c55a19edd39ba85520aa049",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "8e1752670b7744b44c11",
+    "url": "/ToDoList/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "be5fcbeb93d3abb4f80f",
+    "url": "/ToDoList/static/css/main.12cadf36.chunk.css"
+  },
+  {
+    "revision": "8e1752670b7744b44c11",
+    "url": "/ToDoList/static/js/2.374f0333.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoList/static/js/2.374f0333.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be5fcbeb93d3abb4f80f",
+    "url": "/ToDoList/static/js/main.fa4909c9.chunk.js"
+  },
+  {
+    "revision": "ac182baa1b0f739a9d67",
+    "url": "/ToDoList/static/js/runtime-main.23f1db3a.js"
+  }
+]);
